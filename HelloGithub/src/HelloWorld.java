0538ff3da8015b30b076789df4774dac8a6970bd
Cwind233 <50346789+Cwind233@users.noreply.github.com>
2019-10-02 16:28:18 +0800

    Update HelloWorld.java

--- HelloGithub/src/HelloWorld.java
+++ HelloGithub/src/HelloWorld.java
@@ -3,8 +3,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello World£¡");
+        System.out.println("Hello WorldÂ£Â¡");
         //123
+        //123
+        
     }
 
 }
