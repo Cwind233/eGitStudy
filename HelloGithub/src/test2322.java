3f4cbbe5d0a6f81d15a34ab07ec1b94f97c8e542
Cwind233 <657928256@qq.com>
2019-10-03 23:41:00 +0800

    符号测试

--- HelloGithub/src/test2322.java
+++ HelloGithub/src/test2322.java
@@ -4,6 +4,7 @@ public class test2322 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("666!@#$%^&*()_");
+        //666测试一下中文
     }
 
 }
